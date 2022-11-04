3f7f8540b3e1cb2ca87340bfd80852677983c84e
annoinspace <aneesahalmask@gmail.com>
2022-11-04 15:41:41 +0000

    new track number functionality

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
+const addBtn = document.getElementById("add-btn")
+const tracklistTable = document.querySelector("#table-body")
+const modal = document.getElementById("add-track-modal")
+
 function deleteSong() {
   let td = event.target.parentNode
   let tr = td.parentNode
   tr.parentNode.removeChild(tr)
+}
+
+function addTrack() {
+  let newRow = document.createElement("tr")
+  let newTrackNumber = document.getElementById("track-number-modal").value
+  let newTrackTitle = document.getElementById("track-title-modal").value
+  let newArtistName = document.getElementById("artist-name-modal").value
+  let newTrackDuration = document.getElementById("track-duration-modal").value
+
+  newRow.innerHTML = `  
+  <th scope="row">${newTrackNumber}</th>
+  <td>${newTrackTitle}</td>
+  <td>${newArtistName}</td>
+  <td>${newTrackDuration}</td>
+  <td></td>`
+  tracklistTable.appendChild(newRow)
 }
