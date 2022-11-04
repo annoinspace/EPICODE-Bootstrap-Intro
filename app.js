b151a45b21d918b586e23a483c7f96b84cc25444
annoinspace <aneesahalmask@gmail.com>
2022-11-04 16:47:50 +0000

    more work

--- app.js
+++ app.js
@@ -22,4 +22,22 @@ function addTrack() {
   <td>${newTrackDuration}</td>
   <td></td>`
   tracklistTable.appendChild(newRow)
+
+  function clearValues() {
+    let newTrackNumber = document.getElementById("track-number-modal").value
+    let newTrackTitle = document.getElementById("track-title-modal").value
+    let newArtistName = document.getElementById("artist-name-modal").value
+    let newTrackDuration = document.getElementById("track-duration-modal").value
+
+    newTrackNumber.value = ""
+    newTrackTitle.value = ""
+    newArtistName.value = ""
+    newTrackDuration.value = ""
+  }
+}
+
+function changeSong() {
+  let songHeader = document.getElementById("song-title")
+  let td = event.target
+  songHeader.innerHTML = td.innerHTML
 }
